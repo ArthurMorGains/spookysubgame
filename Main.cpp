10e415f62f41b01b1ca64ff5237a4b3ef99d9b59
ArthurMorGains <theronmccombs@gmail.com>
2025-08-04 21:07:47 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,26 @@
 #include "raylib.h"
+#include "raymath.h"
+
 #include "Game.h"
 
 
 int main() {
-	int windowWidth = 800;
-	int windowHeight = 500;
+	int windowWidth = 1000;
+	int windowHeight = 750;
 
-	InitWindow(windowWidth, windowHeight, "Raylib Game!");
-	SetTargetFPS(60);
+	InitWindow(windowWidth, windowHeight, "SpookySubGame");
+	SetTargetFPS(144);
 
 	Game game;
 
 	while (!WindowShouldClose()) {
+		game.Update();
 		BeginDrawing();
+		BeginMode2D(game.cam);
 		ClearBackground(DARKGREEN);
 		game.HandleInput();
 		game.Draw();
+		EndMode2D();
 		EndDrawing();
 	}
 	CloseWindow();
